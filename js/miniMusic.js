961c9750015a19951d820f3cba235a9badab5eab
凤舞九天 <1833608478@qq.com>
2019-12-18 21:41:02 +0800

    上传js

---
+++ js/miniMusic.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    $("#like").css("background-color","#7E57C2");
+    $("#musicHouse").click(function () {
+        $("#musicHouse").css("background-color","#7E57C2");
+        $("#video").css("background-color","");
+        $("#radio").css("background-color","");
+        $("#like").css("background-color","");
+        $("#local").css("background-color","");
+        $("#history").css("background-color","");
+        $("#recentList").css("background-color","");
+    });
+    $("#video").click(function () {
+        $("#musicHouse").css("background-color","");
+        $("#video").css("background-color","#7E57C2");
+        $("#radio").css("background-color","");
+        $("#like").css("background-color","");
+        $("#local").css("background-color","");
+        $("#history").css("background-color","");
+        $("#recentList").css("background-color","");
+    });
+    $("#radio").click(function () {
+        $("#musicHouse").css("background-color","");
+        $("#video").css("background-color","");
+        $("#radio").css("background-color","#7E57C2");
+        $("#like").css("background-color","");
+        $("#local").css("background-color","");
+        $("#history").css("background-color","");
+        $("#recentList").css("background-color","");
+    });
+    $("#like").click(function () {
+        $("#musicHouse").css("background-color","");
+        $("#video").css("background-color","");
+        $("#radio").css("background-color","");
+        $("#like").css("background-color","#7E57C2");
+        $("#local").css("background-color","");
+        $("#history").css("background-color","");
+        $("#recentList").css("background-color","");
+    });
+    $("#local").click(function () {
+        $("#musicHouse").css("background-color","");
+        $("#video").css("background-color","");
+        $("#radio").css("background-color","");
+        $("#like").css("background-color","");
+        $("#local").css("background-color","#7E57C2");
+        $("#history").css("background-color","");
+        $("#recentList").css("background-color","");
+    });
+    $("#history").click(function () {
+        $("#musicHouse").css("background-color","");
+        $("#video").css("background-color","");
+        $("#radio").css("background-color","");
+        $("#like").css("background-color","");
+        $("#local").css("background-color","");
+        $("#history").css("background-color","#7E57C2");
+        $("#recentList").css("background-color","");
+    });
+    $("#recentList").click(function () {
+        $("#musicHouse").css("background-color","");
+        $("#video").css("background-color","");
+        $("#radio").css("background-color","");
+        $("#like").css("background-color","");
+        $("#local").css("background-color","");
+        $("#history").css("background-color","");
+        $("#recentList").css("background-color","#7E57C2");
+    });
+});
